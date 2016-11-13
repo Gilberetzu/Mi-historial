1425b312487f1307c170d8bd56e86ba6285b9711
Gilberetzu <euri.herasme@gmail.com>
2016-11-14 01:26:16 +0800

    v0.3
    algo

---
+++ BAM/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,30 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-inappbrowser": "1.5.0",
+    "cordova-plugin-whitelist": "1.3.0",
+    "phonegap-plugin-barcodescanner": "6.0.3"
+};
+// BOTTOM OF METADATA
+});
